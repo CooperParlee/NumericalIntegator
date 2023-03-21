913d612ddd306bf446a41b6d304739749829712b
Cooper Parlee <cooper@cooperparlee.com>
2023-03-21 19:37:46 -0400

    Test file created

--- src/com/cooperparlee/numerical_integrator/IntegrateTest.java
+++ src/com/cooperparlee/numerical_integrator/IntegrateTest.java
@@ -1,11 +1,38 @@
 package com.cooperparlee.numerical_integrator;
 
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
-
 public class IntegrateTest {
-	public static void main(String[] args) throws ScriptException{
-		System.out.println("Faggot");
+	final static int a = 0;
+	final static int b = 2;
+	final static int n = 10;
+	
+	final static double dx = ((double)b-a)/n;
+	
+	public static void main(String[] args){
+		double sum = 0;
+		
+		System.out.println("Initial Values: a: " + a + " b: " + b + " n: "+ n + " dx: " + dx);
+		
+		for (int i = 0; i <= n; i++) {
+			double fxI;
+			double i_x = i*dx + a;
+			
+			if (i == 0 || i == n) {
+				fxI = fx(i_x);
+			} else {
+				fxI = fx(i_x)*2;
+			}
+			
+			sum += fxI;
+			
+			System.out.println("i: " + i + " x_i: " + i_x + " f(x_I): " + fxI);
+		}
+		
+		System.out.println("Sum: " + sum);
+		double Tn = sum * dx/2;
+		
+	}
+	
+	private static double fx (double x) {
+		return Math.exp(x)/(1.0+Math.pow(x, 2));
 	}
 }
