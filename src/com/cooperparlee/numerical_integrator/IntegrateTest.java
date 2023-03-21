0200f613b9a5127d55e642c0bde7ceb54f9800dc
Cooper Parlee <cooper@cooperparlee.com>
2023-03-20 20:24:10 -0400

    Initial commit

---
+++ src/com/cooperparlee/numerical_integrator/IntegrateTest.java
@@ -0,0 +1,11 @@
+package com.cooperparlee.numerical_integrator;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+public class IntegrateTest {
+	public static void main(String[] args) throws ScriptException{
+		System.out.println("Faggot");
+	}
+}
