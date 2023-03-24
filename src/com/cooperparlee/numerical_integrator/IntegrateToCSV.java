f39a15ef65e55dd9883a09fd4dd06e7c3c7f956c
Cooper Parlee <cooper@cooperparlee.com>
2023-03-23 20:43:36 -0400

    Final iteration, tested if program would work if given a huge precision; creates HUGE memory leak

--- src/com/cooperparlee/numerical_integrator/IntegrateToCSV.java
+++ src/com/cooperparlee/numerical_integrator/IntegrateToCSV.java
@@ -3,31 +3,31 @@ package com.cooperparlee.numerical_integrator;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IntegrateToCSV {
-	final static int a = 0;
-	final static int b = 2;
-	final static int n = 10;
-	
-	
-	final static double dx = ((double)b-a)/n;
 	
 	public IntegrateToCSV () throws IOException
 	{
-		main();
+		long t1 = new Date().getTime();
+		
+		main(0 , 1, 2147483646, "Simpson", 15);
+		main(0 , 1, 2147483646, "Midpoint", 15);
+		main(0 , 1, 2147483646, "Trapezoid", 15);
+		
+		System.out.println("Completed. Took " + (new Date().getTime() - t1) + " millis.");
 	}	
-	public void main() throws IOException{
+	public void main(int a, int b, int n, String intCase, int problemNumber) throws IOException{
+		double dx = ((double)b-a)/n;
+		
 		List<String[]> lines = new ArrayList<>();
 		
 		lines.add(new String[] {
-				 "", "a=", Integer.toString(a), "b=", Integer.toString(b), "n=", Integer.toString(n), "f(x)="
+				 "", "a=", Integer.toString(a), "b=", Integer.toString(b), "n=", Integer.toString(n), "f(x)=", "", "Case", intCase
 		});
 		lines.add(new String[] {
 				"", "i", "x_i", "f(x_i)"
@@ -35,29 +35,76 @@ public class IntegrateToCSV {
 		
 		double sum = 0;
 		
-		for (int i = 0; i <= n; i++) {
-			double fxI;
-			double i_x = i*dx + a;
-			
-			if (i == 0 || i == n) {
-				fxI = fx(i_x);
-			} else {
-				fxI = fx(i_x)*2;
-			}
-			
-			sum += fxI;
-			
-			lines.add(new String[] {"", Integer.toString(i), Double.toString(i_x), Double.toString(fxI)});
+		double integralApprox = 0;
+		
+		switch (intCase) {
+			case "Simpson":
+				if (!isEven(n)) {
+					System.out.println("Error: n is not odd!");
+					return;
+				}
+				for (int i = 0; i <= n; i++) {
+					double fxI;
+					double i_x = i*dx + a;
+					
+					if (i == 0 || i == n) {
+						fxI = fx(i_x);
+					} else if (!isEven(i)){
+						fxI = fx(i_x)*2;
+					} else {
+						fxI = fx(i_x)*4;
+					}
+					
+					sum += fxI;
+					
+					lines.add(new String[] {"", Integer.toString(i), Double.toString(i_x), Double.toString(fxI)});
+				}
+				
+				integralApprox = sum * dx/3;
+				
+			break;
+			case "Trapezoid":
+				for (int i = 0; i <= n; i++) {
+					double fxI;
+					double i_x = i*dx + a;
+					
+					if (i == 0 || i == n) {
+						fxI = fx(i_x);
+					} else {
+						fxI = fx(i_x)*2;
+					}
+					
+					sum += fxI;
+					
+					lines.add(new String[] {"", Integer.toString(i), Double.toString(i_x), Double.toString(fxI)});
+				}
+				
+				integralApprox = sum * dx/2;
+				
+			break;
+			case "Midpoint":
+				for (int i = 1; i <= n; i++) {
+					double fxI;
+					double x_bar = (i + (i-1))*dx/2.0 + a;
+					
+					fxI = fx(x_bar);
+					
+					sum += fxI;
+					
+					lines.add(new String[] {"", Integer.toString(i), Double.toString(x_bar), Double.toString(fxI)});
+				}
+				integralApprox = sum * dx;
+			break;
+			default:
+				System.out.println("That's not a case dumbass.");
+			return;
 		}
 		
-		double Tn = sum * dx/2;
 		
 		lines.add(new String[] {"", "Sum :", Double.toString(sum)});
-		lines.add(new String[] {"", "Tn :", Double.toString(Tn)});
+		lines.add(new String[] {"", "Tn :", Double.toString(integralApprox)});
 		
-		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH.mm.ss");
-		
-		File csvOutputFile = new File("C:\\Users\\cparl\\OneDrive\\Programming Workspaces\\Numerical Integrator\\output\\out " + LocalDateTime.now().format(myFormatObj) + ".csv");
+		File csvOutputFile = new File("C:\\Users\\cparl\\OneDrive\\Programming Workspaces\\Numerical Integrator\\output\\out " + problemNumber +" - " + intCase + " n" + n +".csv");
 	    try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
 	        lines.stream()
 	          .map(this::convertToCSV)
@@ -67,7 +114,7 @@ public class IntegrateToCSV {
 	}
 	
 	private static double fx (double x) {
-		return Math.exp(x)/(1.0+Math.pow(x, 2));
+		return Math.pow(x, 2)/(1+Math.pow(x, 4));
 	}
 	
 	public String convertToCSV(String[] data) {
@@ -83,5 +130,9 @@ public class IntegrateToCSV {
 	        escapedData = "\"" + data + "\"";
 	    }
 	    return escapedData;
+	}
+	
+	public boolean isEven (double value) {
+		return (value / 2.0) == Math.floor(value / 2.0); 
 	}
 }
